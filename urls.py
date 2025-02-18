9b760c82a77d029686aba65fb5a96fa0e6e42c36
Rättchen <57540790+Rattchen@users.noreply.github.com>
2025-02-18 17:38:22 +0100

    Create simple ClanListView

--- urls.py
+++ urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import IndexView, LeaderboardView
+from .views import IndexView, LeaderboardView, ClanListView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('clans', ClanListView.as_view(), name='clan_list'),
     path('leaderboard', LeaderboardView.as_view(), name='leaderboard')
 
 ]
--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from django.db.models import F
 from .models import Clan, ScuffleProfile
 
@@ -10,6 +10,12 @@ class IndexView(TemplateView):
         context['message'] = 'Hi, welcome to Scuffle module!'
         return context
 
+class ClanListView(ListView):
+    # TODO: Most points user + points in total for each clan
+    template_name = 'scuffle/clan_list.html'
+    model = Clan
+    context_object_name = "clans"
+
 class LeaderboardView(TemplateView):
     template_name = 'scuffle/leaderboard.html'
     
@@ -17,9 +23,10 @@ class LeaderboardView(TemplateView):
         context = super().get_context_data(**kwargs)
         clans = Clan.objects.all()
         context['clans'] = clans
-        
+
         for clan in context['clans']:
             clan.leaderboard = clan.people.all().annotate(
                 total_points = F('points')+F('nibbleProfile__points') #calculate manually since it's not a field but a @property
             ).order_by('-total_points')
         return context
+
