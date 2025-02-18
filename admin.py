8d2b5ee38506fc7e4806cd1f9b87eacc90d2fbb3
Rättchen <57540790+Rattchen@users.noreply.github.com>
2025-02-18 16:06:51 +0100

    Create ScuffleProfile and Clan models

--- admin.py
+++ admin.py
@@ -1,1 +1,6 @@
 from django.contrib import admin
+from .models import ScuffleProfile, Clan
+
+
+admin.site.register(ScuffleProfile)
+admin.site.register(Clan)
--- models.py
+++ models.py
@@ -1,1 +1,22 @@
 from django.db import models
+from nibble.models import NibbleProfile
+
+class Clan(models.Model):
+    name = models.CharField(max_length=50)
+    icon = models.CharField(max_length=5, default="🏠")
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class ScuffleProfile(models.Model):
+    nibbleProfile = models.OneToOneField(NibbleProfile, on_delete=models.CASCADE)
+    points = models.IntegerField(default=0)
+    clan = models.ForeignKey(Clan, on_delete=models.CASCADE, null=True, blank=True)
+
+    @property
+    def points_in_total(self):
+        return (NibbleProfile.points + self.points)
+
+    def __str__(self):
+        return f"@{self.nibbleProfile.handle} ({self.nibbleProfile.user.username})"
