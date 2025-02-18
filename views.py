648c1b0f52c69710e71e0525edd592c69158c5d4
Rättchen <57540790+Rattchen@users.noreply.github.com>
2025-02-18 15:34:52 +0100

    Simple view to check routing settings

--- views.py
+++ views.py
@@ -1,1 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class IndexView(TemplateView):
+    template_name = 'scuffle/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['message'] = 'Hi, welcome to Scuffle module!'
+        return context
